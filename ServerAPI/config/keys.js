63ba975ca8d7d70815d52e6a2aef99407f16bef6
Tinkldeath <dropdeadit@gmail.com>
2021-07-14 17:09:33 +0300

    Reconnected db

--- ServerAPI/config/keys.js
+++ ServerAPI/config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoUrl: 'mongodb+srv://root:root@mcbcluster.fygjr.mongodb.net/MCB?retryWrites=true&w=majority'
+    mongoUrl: 'mongodb+srv://root:root@mcbcluster.fygjr.mongodb.net/test'
 }
