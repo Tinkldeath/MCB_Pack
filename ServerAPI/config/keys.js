d9c5e789c634802d7c74e338736de9ccaeac9500
Tinkldeath <dropdeadit@gmail.com>
2021-07-23 16:11:31 +0300

    Authorization and registration fixed

--- ServerAPI/config/keys.js
+++ ServerAPI/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoUrl: 'mongodb+srv://root:root@mcbcluster.fygjr.mongodb.net/MCB'
+    mongoUrl: 'mongodb+srv://root:root@mcbcluster.fygjr.mongodb.net/MCB',
+    jwt: 'dev-jwt' // Константа для токена
 }
--- ServerAPI/controllers/authController.js
+++ ServerAPI/controllers/authController.js
@@ -1,43 +1,71 @@
+const Bcrypt = require('bcryptjs');
+const JWT = require('jsonwebtoken');
 const User = require('./../models/userModel');
+const Keys = require('./../config/keys');
 
 // Логика входа без защиты пароля
 module.exports.login = async function(req,res){
     const reqLogin = req.body.login;
     const reqPas = req.body.password;
-    const found = await User.findOne({login: reqLogin, password: reqPas},function(err){
+    // Ищем такого юзера
+    const found = await User.findOne({login: reqLogin},function(err){
         if(err){
             console.log(err);
             return;
         }
     });
     if(found){
-        res.sendStatus(200);
+        // Проверяем пароль
+        const pasRes = Bcrypt.compareSync(reqPas,found.password);
+        if(pasRes){
+            // Генерируем токен для пользователя, обновляется каждый час
+            const resToken = JWT.sign({
+                _id: found._id,
+                login: found.login,
+                isAdmin: found.isAdmin 
+            }, Keys.jwt, {expiresIn: 60*60});
+            // Отправляем сгенерированный токен
+            res.status(200).json({
+                token: `Bearer ${resToken}`
+            });
+        }
+        else{
+            // Пароли не совпали
+            res.sendStatus(401);
+        }
     }
     else{
         res.sendStatus(404);
     }
 }
 
-// Логика регистрации без защиты пароля
+// Логика регистрации с защитой пароля
 module.exports.register = async function(req,res){
-    const user = new User({
-        login: req.body.login,
-        password: req.body.password,
-        isAdmin: req.body.isAdmin
-    });
-    const found = await User.findOne({login: req.body.login, password: req.body.password},function(err){
+    const found = await User.findOne({login: req.body.login},function(err){
         if(err){
             console.log(err);
             return;
         }
-    })
+    });
     if(found){
         res.sendStatus(409);
     }
     else{
-        user.save().then(() => {
-            console.log('User created');
+        const salt = Bcrypt.genSaltSync(10);
+        const pas = req.body.password;
+        const user = new User({
+            login: req.body.login,
+            password: Bcrypt.hashSync(pas,salt),
+            isAdmin: req.body.isAdmin
         });
-        res.sendStatus(200);
+        try {
+            await user.save().then(() => {
+                console.log('User created');
+            });
+            res.sendStatus(201);
+        } catch (err) {
+            console.log(err);
+            res.sendStatus(500);
+        }
     }
 }
