6c1d517c44721c50622c48c602638929fba64fef
Tinkldeath <dropdeadit@gmail.com>
2021-07-25 19:10:27 +0300

    Added controllers

--- ServerAPI/controllers/categoriesController.js
+++ ServerAPI/controllers/categoriesController.js
@@ -1,17 +1,109 @@
 const Category = require('./../models/categoriesModel');
 
 module.exports.getAll = async function(req,res){
-    // Получение всех категорий
+    await Category.find({},function(err,docs){
+        if(err){
+            console.log(err);
+            res.json({
+                message: 'DB error'
+            });
+        }
+        else{
+            res.send(docs);
+        }
+    })
 }
 
 module.exports.addCategory = async function(req,res){
-    // Добавление категории
+    const reqName = req.body.name;
+    const reqDescription = req.body.description;
+    const category = new Category({
+        name: reqName,
+        description: reqDescription
+    });
+    const found = await Category.findOne({name: reqName},(err)=>{
+        if(err){
+            console.log(err);
+            res.json({
+                message: 'DB error'
+            });
+        }
+    });
+    if(found){
+        res.json({
+            message: 'Conflict'
+        });
+        return;
+    }
+    else{
+        try {
+            await category.save().then(() => {
+                console.log('Category created');
+            });
+            res.json({
+                message: 'Created'
+            });
+        } catch (err) {
+            console.log(err);
+            res.json({
+                message: 'Server error'
+            });
+        }
+    }
 }
 
 module.exports.removeCategory = async function(req,res){
-    // Удаление категории
+    const reqId = req.body.id;
+    try {
+        await Category.findByIdAndDelete(reqId,(err) => {
+            if(err){
+                console.log(err);
+                res.json({
+                    message: 'DB error'
+                });
+            }
+            else{
+                console.log('Category deleted');
+                res.json({
+                    message: 'Deleted'
+                });
+            }
+        });
+    } catch (err) {
+        if(err){
+            console.log(err);
+            res.json({
+                message: 'Server error'
+            });
+        }
+    }
 }
 
 module.exports.updateCategory = async function(req,res){
-    // Обновление категории
+    const reqId = req.body.id;
+    const reqName = req.body.name;
+    const reqDescription = req.body.description;
+    try {
+        await Category.findByIdAndUpdate(reqId,{name: reqName, description: reqDescription},(err) => {
+            if(err){
+                console.log(err);
+                res.json({
+                    message: 'DB error'
+                });
+            }
+            else{
+                console.log(`Category ${reqId} updated`);
+                res.json({
+                    message: 'Updated'
+                });
+            }
+        });
+    } catch (err) {
+        if(err){
+            console.log(err);
+            res.json({
+                message: 'Server error'
+            });
+        }
+    }
 }
